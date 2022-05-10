fd84956c8c7e281abb3ad98305e6cd5b8c8906af
M Afzaal Afzal <afzaal1721@gmail.com>
2022-05-10 15:05:54 +0500

    added the fonts

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
+import Image from 'next/image';
 
-interface Props {}
+interface Props {
+}
 
-const Header:React.FC<Props> = () => {
+const Header: React.FC<Props> = () => {
     return (
-        <div>
-            Header
+        <div className={`bg-black`}>
+            <div className={`container flex items-center justify-between`}>
+
+                {/*     Header Logo     */}
+                <div>
+                    <Image src={'/logo.svg'} width={144} height={34}/>
+                </div>
+
+                {/*     Center Links    */}
+                <div>
+                    center links
+                </div>
+
+                {/*     Right side buttons      */}
+                <div>
+                    Right Button
+                </div>
+
+            </div>
         </div>
     );
 };
---
+++ pages/_document.tsx
@@ -0,0 +1,117 @@
+import React from 'react'
+import Document, {DocumentContext, Html, Head, Main, NextScript} from 'next/document'
+
+const MyDocument: React.FC<Document> = () => {
+    return (
+        <html>
+
+        <Head>
+
+            {/*     Importing the fonts     */}
+            <link
+                rel="preload"
+                href="/fonts/Planer-Bold.ttf"
+                as="font"
+                type="font/woff"
+                crossOrigin=""
+            />
+            <link
+                rel="preload"
+                href="/fonts/Planer-DemiBold.ttf"
+                as="font"
+                type="font/woff"
+                crossOrigin=""
+            />
+
+            <link
+                rel="preload"
+                href="/fonts/Planer-DemiBoldItalic.ttf"
+                as="font"
+                type="font/woff"
+                crossOrigin=""
+            />
+
+            <link
+                rel="preload"
+                href="/fonts/Planer-ExtraBold.ttf"
+                as="font"
+                type="font/woff"
+                crossOrigin=""
+            />
+
+            <link
+                rel="preload"
+                href="/fonts/Planer-ExtraBoldItalic.ttf"
+                as="font"
+                type="font/woff"
+                crossOrigin=""
+            />
+
+            <link
+                rel="preload"
+                href="/fonts/Planer-ExtraLight.ttf"
+                as="font"
+                type="font/woff"
+                crossOrigin=""
+            />
+
+            <link
+                rel="preload"
+                href="/fonts/Planer-Italic.ttf"
+                as="font"
+                type="font/woff"
+                crossOrigin=""
+            />
+
+            <link
+                rel="preload"
+                href="/fonts/Planer-Light.ttf"
+                as="font"
+                type="font/woff"
+                crossOrigin=""
+            />
+
+            <link
+                rel="preload"
+                href="/fonts/Planer-Medium.ttf"
+                as="font"
+                type="font/woff"
+                crossOrigin=""
+            />
+
+            <link
+                rel="preload"
+                href="/fonts/Planer-MediumItalic.ttf"
+                as="font"
+                type="font/woff"
+                crossOrigin=""
+            />
+
+            <link
+                rel="preload"
+                href="/fonts/Planer-Regular.ttf"
+                as="font"
+                type="font/woff"
+                crossOrigin=""
+            />
+
+            <link
+                rel="preload"
+                href="/fonts/Planer-Regular.otf"
+                as="font"
+                type="font/woff"
+                crossOrigin=""
+            />
+
+        </Head>
+
+        <body>
+        <Main/>
+        </body>
+
+        <NextScript/>
+        </html>
+    )
+}
+
+export default MyDocument
