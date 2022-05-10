6932eb47c2f0396f6d5699d8ae003cc3d83ce274
M Afzaal Afzal <afzaal1721@gmail.com>
2022-05-11 02:29:01 +0500

    done with the hero section

--- components/HomePage/HeroSection.tsx
+++ components/HomePage/HeroSection.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
+import ButtonPrimary from "../common/Buttons/ButtonPrimary";
 
 interface Props {}
 
 const HeroSection:React.FC<{}> = () => {
     return (
-        <div>
-            Hero Section
+        <div className={`py-20 bg-black`}>
+            <div className={`container`}>
+                <h1 className={`max-w-2xl mx-auto text-accent font-Planer-DemiBold text-[40px] md:text-[76px] text-center leading-[76px]`}>
+                    Auto-Staking & Compounding From the Future
+                </h1>
+
+                <p className={`mt-8 max-w-[760px] mx-auto text-xl text-center font-Planer-Medium leading-[32px]`}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse venenatis eros erat, a dapibus enim vulputate eu. Donec tristique ex sed leo rutrum aliquam ornare at quam
+                </p>
+
+                <div className={`flex flex-col md:flex-row justify-center items-center space-y-8 md:space-y-0 space-x-8 mt-16`}>
+                    <ButtonPrimary bgGreen withLeftCut btnLg>
+                        Click here to buy tytan
+                    </ButtonPrimary>
+
+                    <ButtonPrimary withRightCut btnLg>
+                        Go to the charts
+                    </ButtonPrimary>
+
+                </div>
+
+            </div>
         </div>
     );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,36 @@
-import type { NextPage } from 'next'
+import type {NextPage} from 'next'
 import {
-  AutoStackingProtocolsSection, CompetitiveAdvantagesSection,
-  CountdownSection, FaqSection,
-  HeroSection, HowItWorksSection,
-  HowMuchCanIEarnSection, JoinOurDiscordSection, TeamSection, TokenomicsSection, TytanAutoLiquiditySection, VideSection
+    AutoStackingProtocolsSection,
+    CompetitiveAdvantagesSection,
+    CountdownSection,
+    FaqSection,
+    HeroSection,
+    HowItWorksSection,
+    HowMuchCanIEarnSection,
+    JoinOurDiscordSection,
+    TeamSection,
+    TokenomicsSection,
+    TytanAutoLiquiditySection,
+    VideSection
 } from "../components/HomePage";
 
 const Home: NextPage = () => {
-  return (
-    <div className="">
-        <HeroSection/>
-        <CountdownSection/>
-      <AutoStackingProtocolsSection/>
-      <HowMuchCanIEarnSection/>
-      <HowItWorksSection/>
-      <JoinOurDiscordSection/>
-      <TytanAutoLiquiditySection/>
-      <TokenomicsSection/>
-      <CompetitiveAdvantagesSection/>
-      <VideSection/>
-      <TeamSection/>
-      <FaqSection/>
-    </div>
-  )
+    return (
+        <div className="">
+            <HeroSection/>
+            <CountdownSection/>
+            <AutoStackingProtocolsSection/>
+            <HowMuchCanIEarnSection/>
+            <HowItWorksSection/>
+            <JoinOurDiscordSection/>
+            <TytanAutoLiquiditySection/>
+            <TokenomicsSection/>
+            <CompetitiveAdvantagesSection/>
+            <VideSection/>
+            <TeamSection/>
+            <FaqSection/>
+        </div>
+    )
 }
 
 export default Home
