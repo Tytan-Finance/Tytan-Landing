c76755dbfcbd5e650573a02b2b795b9b671de775
M Afzaal Afzal <afzaal1721@gmail.com>
2022-05-12 01:33:49 +0500

    done with the team section

--- components/HomePage/TeamSection.tsx
+++ components/HomePage/TeamSection.tsx
@@ -1,11 +1,71 @@
 import React from 'react';
+import {ResponsiveH1} from "../common";
+import Image from 'next/image';
 
-interface Props {}
+const teamMembers = [
+    {
+        id: 1,
+        name: 'Afzaal',
+        imgSrc: '/team/1.jpg',
+    },{
+        id: 2,
+        name: 'Afzaal',
+        imgSrc: '/team/1.jpg',
+    },{
+        id: 3,
+        name: 'Afzaal',
+        imgSrc: '/team/1.jpg',
+    },{
+        id: 4,
+        name: 'Afzaal',
+        imgSrc: '/team/2.jpg',
+    },{
+        id: 5,
+        name: 'Test 5',
+        imgSrc: '/team/2.jpg',
+    },{
+        id: 6,
+        name: 'Test 6',
+        imgSrc: '/team/1.jpg',
+    },{
+        id: 7,
+        name: 'Test 7',
+        imgSrc: '/team/2.jpg',
+    },{
+        id: 8,
+        name: 'Test 8',
+        imgSrc: '/team/1.jpg',
+    },
+]
 
-const TeamSection:React.FC<Props> = () => {
+interface Props {
+}
+
+const TeamSection: React.FC<Props> = () => {
     return (
-        <div>
-            Team section
+        <div className={`bg-black pb-20`}>
+            <div className={`max-w-[1000px] mx-auto p-4`}>
+                <ResponsiveH1 className={`text-accent text-center`}>
+                    TEAM
+                </ResponsiveH1>
+
+                <div className={`flex flex-wrap justify-center mt-6 items-center gap-5`}>
+                    {
+                        teamMembers.map(({id,imgSrc,name}) => (
+                            <div key={id} className={`max-w-[225px] w-full`}>
+                                <div className={`relative w-[225px] h-[225px]`}>
+                                    <Image src={imgSrc} layout={'fill'} objectFit={'cover'}/>
+                                </div>
+                                <p className={`font-Planer-Medium uppercase text-[32px]`}>
+                                    {name}
+                                </p>
+                            </div>
+                        ))
+                    }
+
+                </div>
+
+            </div>
         </div>
     );
 };
--- components/HomePage/VideSection.tsx
+++ components/HomePage/VideSection.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import ReactPlayer from "react-player";
 
-interface Props {}
+interface Props {
+}
 
-const VideSection:React.FC<Props> = () => {
+const VideSection: React.FC<Props> = () => {
     return (
-        <div>
-            Video section
+        <div className={`bg-black py-20`}>
+            <div className={`max-w-[1000px] mx-auto p-4`}>
+                <div className='player-wrapper'>
+                    <ReactPlayer
+                        controls={true}
+                        className='react-player'
+                        url='https://www.youtube.com/watch?v=ysz5S6PUM-U'
+                        width='100%'
+                        height='100%'
+                    />
+                </div>
+            </div>
         </div>
     );
 };
