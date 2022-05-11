73a558512385eb4909d33fef76e97651608a161e
M Afzaal Afzal <afzaal1721@gmail.com>
2022-05-11 20:40:49 +0500

    done with the join our discord section

--- components/HomePage/JoinOurDiscordSection.tsx
+++ components/HomePage/JoinOurDiscordSection.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import {ResponsiveH1} from "../common";
+import Image from 'next/image';
 
 interface Props {}
 
 const JoinOurDiscordSection : React.FC<Props> = () => {
     return (
-        <div>
-            JoinOurDiscordSection
+        <div className={`bg-black pb-20 leading-[50px] space-x-4 text-accent flex items-center justify-center`}>
+            {/*     Logo Image      */}
+            <div className={`flex items-center justify-center`}>
+                <Image src={'/homePage/discord.svg'} width={91.71} height={103.69}/>
+            </div>
+
+            {/*     Text to join discord    */}
+            <div>
+                <ResponsiveH1>
+                    Join our <br/>
+                    Discord Channel
+                </ResponsiveH1>
+            </div>
         </div>
     );
 };
