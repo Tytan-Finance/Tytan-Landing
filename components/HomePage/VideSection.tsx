a2b873cd4fc026fb46a0ea4c8982a9cf0e9cfae9
M Afzaal Afzal <afzaal1721@gmail.com>
2022-05-16 18:00:27 +0500

    done with the video part

--- components/HomePage/VideSection.tsx
+++ components/HomePage/VideSection.tsx
@@ -8,20 +8,86 @@ interface Props {
 const VideSection: React.FC<Props> = () => {
     return (
         <div className={`py-20`}>
-            <Fade triggerOnce>
+            {/*<Fade triggerOnce>*/}
 
             <div className={`max-w-[1000px] mx-auto p-4`}>
-                <div className='player-wrapper'>
-                    <ReactPlayer
-                        controls={true}
-                        className='react-player'
-                        url='https://www.youtube.com/watch?v=aeN1QeFDBjQ'
-                        width='100%'
-                        height='100%'
-                    />
+                <div className={`flex items-center justify-center flex-wrap gap-5`}>
+                    <div className={`max-w-[300px] w-full`}>
+                        <div className='player-wrapper'>
+                            <ReactPlayer
+                                controls={true}
+                                className='react-player'
+                                url='https://www.youtube.com/watch?v=aeN1QeFDBjQ'
+                                width='100%'
+                                height='100%'
+                            />
+                        </div>
+                    </div>
+
+                    <div className={`max-w-[300px] w-full`}>
+                        <div className='player-wrapper'>
+                            <ReactPlayer
+                                controls={true}
+                                className='react-player'
+                                url='https://www.youtube.com/watch?v=aeN1QeFDBjQ'
+                                width='100%'
+                                height='100%'
+                            />
+                        </div>
+                    </div>
+
+                    <div className={`max-w-[300px] w-full`}>
+                        <div className='player-wrapper'>
+                            <ReactPlayer
+                                controls={true}
+                                className='react-player'
+                                url='https://www.youtube.com/watch?v=aeN1QeFDBjQ'
+                                width='100%'
+                                height='100%'
+                            />
+                        </div>
+                    </div>
+
+                    <div className={`max-w-[300px] w-full`}>
+                        <div className='player-wrapper'>
+                            <ReactPlayer
+                                controls={true}
+                                className='react-player'
+                                url='https://www.youtube.com/watch?v=aeN1QeFDBjQ'
+                                width='100%'
+                                height='100%'
+                            />
+                        </div>
+                    </div>
+
+                    <div className={`max-w-[300px] w-full`}>
+                        <div className='player-wrapper'>
+                            <ReactPlayer
+                                controls={true}
+                                className='react-player'
+                                url='https://www.youtube.com/watch?v=aeN1QeFDBjQ'
+                                width='100%'
+                                height='100%'
+                            />
+                        </div>
+                    </div>
+
+                    <div className={`max-w-[300px] w-full`}>
+                        <div className='player-wrapper'>
+                            <ReactPlayer
+                                controls={true}
+                                className='react-player'
+                                url='https://www.youtube.com/watch?v=aeN1QeFDBjQ'
+                                width='100%'
+                                height='100%'
+                            />
+                        </div>
+                    </div>
+
                 </div>
+
             </div>
-            </Fade>
+            {/*</Fade>*/}
         </div>
     );
 };
