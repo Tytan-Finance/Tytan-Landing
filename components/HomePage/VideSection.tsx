f0e2b05469fcffd627e2ef13720fd271b70a49ce
M Afzaal Afzal <afzaal1721@gmail.com>
2022-05-19 14:32:49 +0500

    added the react player youtube

--- components/HomePage/VideSection.tsx
+++ components/HomePage/VideSection.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import ReactPlayer from "react-player";
-import {Fade} from "react-awesome-reveal";
+import ReactPlayer from 'react-player/youtube'
 
 interface Props {
 }
